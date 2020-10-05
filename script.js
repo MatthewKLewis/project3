c878425afbe6d46506db0d4bb835b1bf80a3a568
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-05 19:34:22 -0400

    Requirements met. API: Star Wars API. UI: Modal view.

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
 /*
- * Load more DIVs on scroll
+ * Load more grid DIVs on scroll
  * 
  */
 
+const api_url = "http://swapi.dev/api/planets/";
 
-// api url 
-const api_url = "https://api.thecatapi.com/v1/images/search";
-let img = [];
-const grid = document.querySelector('.grid');
+let grid = document.querySelector('.grid');
+let modal = document.querySelector('.modal')
+let modalName = document.getElementById('modalName');
+let modalClimate = document.getElementById('modalClimate');
+let modalDiameter = document.getElementById('modalDiameter');
+let modalGravity = document.getElementById('modalGravity');
+let modalOPeriod = document.getElementById('modalOPeriod');
+let modalPopulation = document.getElementById('modalPopulation');
+let modalTerrain = document.getElementById('modalTerrain');
 
-let setAttributeOfImage = function(attribute, value) {
-    catIMG.setAttribute(attribute, value);
-}
+let expanded = false;
 
-//DOM Declarations
-let catIMG = document.getElementById("randomCatImage");
-
-// Defining async function 
-async function getapi(url, functionX) { 
-    
+async function populateButtonfromAPIinfo() { 
     // Storing response, WHEN IT COMES
-    const response = await fetch(url); //FETCH MAKES A RESPONSE
+    const response = await fetch(api_url + (Math.floor(Math.random() * 50) + 1)); //FETCH MAKES A RESPONSE
     
     // DATA IS THE RESPONSE, AFTER IT COMES, AFTER IT IS FORMATTED
-    img = await response.json(); 
-    console.log(img[0].url); //THE IMAGE IS ARRAY ITEM 0 FROM A SERVER RESPONSE, the URL is a PROPERTY
-    functionX('src', img[0].url);
+    data = await response.json();
+
+    //Set up the button
+    tempDiv = document.createElement('button');
+    tempDiv.appendChild(document.createTextNode(data.name.toLowerCase()));
+    tempDiv.classList.add('gridItem');
+
+    //Cheeky data smuggling
+    tempDiv.aaa = data;
+
+    //Add click behavior to the button which expands the modal
+    tempDiv.addEventListener('click', expand);
+    tempDiv.addEventListener('click', populateFields);
+
+    //Put the button into the parent grid
+    grid.appendChild(tempDiv);
 }
 
-// Calling the async function
-getapi(api_url, setAttributeOfImage);
+function expand() {
+    grid.classList.toggle('transparent');
+    modal.classList.toggle('hidden');
+}
 
+function populateFields() {
+    modalName.innerText = this.aaa.name.toLowerCase();
+    modalClimate.innerText = this.aaa.climate
+    modalDiameter.innerText = this.aaa.diamter
+    modalGravity.innerText = this.aaa.gravity
+    modalOPeriod.innerText = this.aaa.orbital_period
+    modalPopulation.innerText = this.aaa.population
+    modalTerrain.innerText = this.aaa.terrain;
+}
 
-
-//START
-//Create 8 Grid Elements
-for (let i = 0; i < 7; i++) {
-    var tempDiv = document.createElement('div')
-    tempDiv.classList.add('grid-item')
-
-    grid.appendChild(tempDiv);
+//START ---------------------------------------------------------
+//Create Grid Elements
+for (let i = 0; i < 8; i++) {
+    //create button with name and data from API and add it to 'grid'
+    populateButtonfromAPIinfo();
 }
